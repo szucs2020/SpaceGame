786153b6f2dcb21742917ce554a22543a700172d
polyal <lorant.polya@gmail.com>
2016-09-18 00:41:21 -0400

    Minimal Fire

---
+++ Assets/Scripts/ParticleEmitter/Particle.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particle : MonoBehaviour {
+
+    private float speed;
+    private float angle;
+    private float lifeSpan;
+    private float tAlive;
+    private float minSize;
+    private float maxSize;
+    float startTime;
+    Vector3 prevSize = new Vector3(0.3f, 0.3f, 1f);
+    private bool shrink = false;
+
+
+    void Update()
+    {
+        flickerFlame();
+    }
+
+    public void initialize(ParticleTypes type)
+    {
+        if (type == ParticleTypes.Water)
+        {
+            speed = 10;
+            angle = 90 * Mathf.PI / 180;
+            lifeSpan = 10;
+        }
+        else if (type == ParticleTypes.Fire)
+        {
+            speed = 5;
+            angle = 90 * Mathf.PI / 180;
+            lifeSpan = 5f;
+            tAlive = 0;
+            minSize = 0.3f;
+            maxSize = 0.2f;
+            startTime = Time.time;
+        }
+    }
+
+    private void flickerFlame()
+    {
+        if (shrink == false)
+        {
+            transform.localScale += new Vector3(0.6f * Time.deltaTime, 0.6f * Time.deltaTime, 0f);
+
+            if (this.transform.localScale.x >= 0.6f)
+            {
+                shrink = true;
+            }
+        }
+        else if (shrink == true)
+        {
+            transform.localScale -= new Vector3(0.6f * Time.deltaTime, 0.6f * Time.deltaTime, 0f);
+
+            if (this.transform.localScale.x <= 0.4f)
+            {
+                shrink = false;
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        this.transform.localPosition = new Vector3(0, 0, 0);
+        return;
+    }
+
+    public void SetSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+    public void SetAngle(float angle)
+    {
+        this.angle = angle;
+    }
+
+    public void SetLifeSpan(float lifeSpan)
+    {
+        this.lifeSpan = lifeSpan;
+    }
+
+    public void SettAlive(float tAlive)
+    {
+        this.tAlive = tAlive;
+    }
+
+    public float GetSpeed()
+    {
+        return this.speed;
+    }
+
+    public float GetAngle()
+    {
+        return this.angle;
+    }
+
+    public float GetLifespan()
+    {
+        return this.lifeSpan;
+    }
+
+    public float GettAlive()
+    {
+        return this.tAlive;
+    }
+
+    public float GetMinSize()
+    {
+        return this.minSize;
+    }
+
+    public float GetMaxSize()
+    {
+        return this.maxSize;
+    }
+
+    public enum ParticleTypes
+    {
+        Water,
+        Mist,
+        Bullet,
+        Blood,
+        Fire,
+        Flame
+    }
+}
---
+++ Assets/Scripts/ParticleEmitter/ParticleEmitterScript.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ParticleEmitterScript : MonoBehaviour {
+
+    float xPos;
+    float ypos;
+    float angle;
+    float speed;
+    float rate;
+    float dt;
+    float prevTime;
+    float lifeSpan;
+    float amount;
+    int amountExists;
+
+    List<Particle> particles;
+
+    public Particle particle;
+
+    // Use this for initialization
+    void Start () {
+        //Initialze Particle
+
+        xPos = 0;
+        ypos = 0;
+        rate = 60;
+        prevTime = Time.time;
+
+        particle.initialize(Particle.ParticleTypes.Fire);
+
+        amount = particle.GetLifespan() * rate;
+        amountExists = 0;
+
+        particles = CreateParticles((int)amount, Particle.ParticleTypes.Fire);
+
+	}
+	
+    
+	// Update is called once per frame
+	void Update () {
+        //update particle position and properties
+        float velocityx;
+        float velocityy;
+
+        dt = Time.time - prevTime;
+
+        if (dt >= rate / 60)
+        {
+            amountExists++;
+
+            prevTime = Time.time;
+        }
+
+        for (int i = 0; i < amountExists && amountExists < amount; i++)
+        {
+            //if (i < amountExists)
+            //{
+            //    //Particle newParticle;
+            //    //newParticle = (Particle)Instantiate(particle, transform.position, Quaternion.identity);
+            //    particles.Add((Particle)Instantiate(particle, transform.position, Quaternion.identity));
+            //    //particles[i] = (Particle)Instantiate(particle, transform.position, Quaternion.identity);
+            //    particles[i].transform.SetParent(transform, false);
+            //    particles[i].transform.localPosition = new Vector3(0, 0, 0);
+
+            //    particles[i].initialize(Particle.ParticleTypes.Fire);
+            //}
+
+            particles[i].SettAlive(particles[i].GettAlive() + dt);
+
+            if (particles[i].GetLifespan() >= particles[i].GettAlive())
+            {
+                particles[i].Reset();
+            }
+            velocityx = particles[i].GetSpeed() * Mathf.Cos(particles[i].GetAngle());
+            velocityy = particles[i].GetSpeed() * Mathf.Sin(particles[i].GetAngle());
+
+            particles[i].transform.Translate(velocityx * Time.deltaTime, velocityy * Time.deltaTime, 0);
+            
+        }
+
+    }
+
+    private List<Particle> CreateParticles(int amount, Particle.ParticleTypes type)
+    {
+        List<Particle> particles = new List<Particle>();
+
+        for (int i = 0; i < amount - 1; i++)
+        {
+            particles.Add((Particle)Instantiate(particle, transform.position, Quaternion.identity));
+            particles[i].transform.SetParent(transform, false);
+            particles[i].transform.localPosition = new Vector3(0, 0, 0);
+
+            particles[i].initialize(type);
+        }
+
+        return particles;
+    }
+}
