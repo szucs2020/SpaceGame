f70383f81e7bbbea0625a5d346dd0cd5d9eedac8
lpolya <lpolya@uoguelph.ca>
2016-09-24 16:06:46 -0400

    AI Keeps Distance from Player
    If the AI and Player are on the same platform then the AI keeps their
    distance

--- Assets/Scripts/AI/AIController.cs
+++ Assets/Scripts/AI/AIController.cs
@@ -34,7 +34,75 @@ public class AIController : MonoBehaviour {
 		buttonPressedJumped = false;
 	}
 
-	private void getClosestNodeToPlayer() {
+	double timedelta = 0;
+	// Update is called once per frame
+	void Update () {
+
+		//getClosestNodeToPlayer ();
+
+		timedelta += Time.deltaTime;
+		if (timedelta < 2) {
+			return;
+		}
+
+		if (AI.currentPlatform == player.GetComponent<Player> ().currentPlatform) {
+
+			float variablePos = Random.Range(-5f, 5f);
+
+			if (player.transform.position.x < transform.position.x && transform.position.x - player.transform.position.x < 30f) {
+				path.Clear ();
+				target = player.GetComponent<Node> ();
+				Move (player.transform.position + new Vector3 (variablePos + 20f, 0, 0));
+			} else if (player.transform.position.x > transform.position.x && player.transform.position.x - transform.position.x < 30f) {
+				path.Clear ();
+				target = player.GetComponent<Node> ();
+				Move (player.transform.position - new Vector3 (variablePos + 20f, 0, 0));
+			}
+		} else if (target != null) {
+			MoveToPlayersPlatform ();
+		} else {
+			print ("Set Movement Axis");
+			ReCalcPath ();
+			//AI.setMovementAxis (new Vector2 (0, 0));
+		}
+	}
+
+
+	/*MoveToPlayersPlatform should be broken up into another function which can be called from
+	 * the else of line 119 so code isn't duplicated
+	 * I guess just separate the firs if
+	 */
+	private void MoveToPlayersPlatform () {
+
+		AI.setbuttonPressedJump (false);
+
+		if(Mathf.Abs(target.transform.position.x - transform.position.x) < .5f) {
+
+			if (path.Count == 0) {
+				target = null;
+				return;
+			}
+
+			target = path [0];
+			path.RemoveAt (0);
+		}
+
+		Move (target.transform.position);
+	}
+
+	void Move(Vector3 target) {
+		if(target.y > transform.position.y + 2 /*&& Mathf.Abs(target.x - transform.position.x) < 15f*/) {
+			AI.setbuttonPressedJump (true);
+		}
+
+		if (target.x < transform.position.x) {
+			AI.setMovementAxis (new Vector2 (-1, 1));
+		} else {
+			AI.setMovementAxis (new Vector2 (1, 1));
+		}
+	}
+
+	void ReCalcPath() {
 		Node closestNode = null;
 
 		/*If path is empty if Player can't be found find closest nodes to AI and Player
@@ -69,98 +137,39 @@ public class AIController : MonoBehaviour {
 			path = pathFinder.FindShortestPath (player.transform.position);
 			target = path [0];
 			path.RemoveAt (0);
-			return;
-		}
-		float distToTarget = (player.transform.position - path [path.Count - 1].transform.position).sqrMagnitude;
-
-		for (int i = 0; i < path [path.Count - 1].neighbour.Count; i++) {
-			float dist = (player.transform.position - path [path.Count - 1].neighbour [i].transform.position).sqrMagnitude;
-
-
-			Vector3 nodeToNeighbour = path [path.Count - 1].neighbour [i].transform.position - path [path.Count - 1].transform.position;
-			Vector3 nodeToPlayer = player.transform.position - path [path.Count - 1].transform.position;
-
-			float nodeToNeighDirX = (nodeToNeighbour / nodeToPlayer.magnitude).x;
-			float nodeToPlayDirX = (nodeToPlayer / nodeToPlayer.magnitude).x;
-			float nodeToNeighDirY = (nodeToNeighbour / nodeToPlayer.magnitude).y;
-			float nodeToPlayDirY = (nodeToPlayer / nodeToPlayer.magnitude).y;
-
-			if(dist < distToTarget ) {
-				distToTarget = dist;
-				closestNode = path [path.Count - 1].neighbour [i];
-			}
-		}
-
-		if (closestNode != null && !closestNode.getInPath ()) {
-			closestNode.setInPath (true);
-			path [path.Count - 1].setColour (Color.yellow);
-			path.Add (closestNode);
-			path [path.Count - 1].setColour (Color.red);
-		} else if (closestNode != null && closestNode.getInPath ()) {
-			closestNode.setColour (Color.magenta);
-			closestNode.setInPath (false);
-			path.RemoveAt (path.Count - 1);
-		}
-	}
-
-	double timedelta = 0;
-	// Update is called once per frame
-	void Update () {
-
-		//getClosestNodeToPlayer ();
-
-		timedelta += Time.deltaTime;
-		if (timedelta < 2) {
-			return;
-		}
-
-		if (AI.currentPlatform == player.GetComponent<Player> ().currentPlatform) {
-			Debug.Log (Mathf.Abs(player.transform.position.x - transform.position.x) );
-
-			if (player.transform.position.x > transform.position.x && player.transform.position.x - transform.position.x < 30f) {
-				path.Clear ();
-				target = player.GetComponent<Node> ();
-				MoveToPlayersPlatform ();
-			}
-		} else if (target != null) {
-			MoveToPlayersPlatform ();
-		} else {
-			print ("Set Movement Axis");
-			AI.setMovementAxis (new Vector2 (0, 0));
 		}
 	}
 
 
-	/*MoveToPlayersPlatform should be broken up into another function which can be called from
-	 * the else of line 119 so code isn't duplicated
-	 * I guess just separate the firs if
-	 */
-	private void MoveToPlayersPlatform () {
 
-		AI.setbuttonPressedJump (false);
 
-		if(Mathf.Abs(target.transform.position.x - transform.position.x) < .5f) {
 
-			if (path.Count == 0) {
-				target = null;
-				return;
-			}
 
-			target = path [0];
-			path.RemoveAt (0);
-		}
 
-		if(target.transform.position.y > transform.position.y + 2 /*&& Mathf.Abs(target.transform.position.x - transform.position.x) < 15f*/) {
-			AI.setbuttonPressedJump (true);
-		}
 
-		if (target.transform.position.x < transform.position.x) {
-			AI.setMovementAxis (new Vector2 (-1, 1));
-		} else {
-			AI.setMovementAxis (new Vector2 (1, 1));
-		}
 
-	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*************
+ * 
+ * 
+ * Previous Implementation
+ * 
+ */
 
 	bool inAir = false;
 	double deltaTime = 0;
@@ -250,6 +259,75 @@ public class AIController : MonoBehaviour {
 			AI.setMovementAxis (new Vector2 (-1, 1));
 		} else {
 			AI.setMovementAxis (new Vector2 (1, 1));
+		}
+	}
+
+	private void getClosestNodeToPlayer() {
+		Node closestNode = null;
+
+		/*If path is empty if Player can't be found find closest nodes to AI and Player
+		 * and run AStar again*/
+		if (path.Count == 0 && player != null) {
+
+			Node closestToAI = null;
+			Node closestToPlayer = null;
+			float dist = float.MaxValue;
+			float smallestDist = float.MaxValue;
+
+			for (int i = 0; i < pathFinder.nodes.Count; i++) {
+				dist = (AI.transform.position - pathFinder.nodes[i].transform.position).sqrMagnitude;
+				if (dist < smallestDist) {
+					smallestDist = dist;
+					closestToAI = pathFinder.nodes [i];
+				}
+			} pathFinder.startNode = closestToAI;
+
+			dist = float.MaxValue;
+			smallestDist = float.MaxValue;
+
+			Vector3 playerPos = player.transform.position - new Vector3 (0, player.transform.position.y / 2, 0);
+			for (int i = 0; i < pathFinder.nodes.Count; i++) {
+				dist = (playerPos - pathFinder.nodes[i].transform.position).sqrMagnitude;
+				if (dist < smallestDist) {
+					smallestDist = dist;
+					closestToPlayer = pathFinder.nodes [i];
+				}
+			} pathFinder.target = closestToPlayer;
+
+			path = pathFinder.FindShortestPath (player.transform.position);
+			target = path [0];
+			path.RemoveAt (0);
+			return;
+		}
+		float distToTarget = (player.transform.position - path [path.Count - 1].transform.position).sqrMagnitude;
+
+		for (int i = 0; i < path [path.Count - 1].neighbour.Count; i++) {
+			float dist = (player.transform.position - path [path.Count - 1].neighbour [i].transform.position).sqrMagnitude;
+
+
+			Vector3 nodeToNeighbour = path [path.Count - 1].neighbour [i].transform.position - path [path.Count - 1].transform.position;
+			Vector3 nodeToPlayer = player.transform.position - path [path.Count - 1].transform.position;
+
+			float nodeToNeighDirX = (nodeToNeighbour / nodeToPlayer.magnitude).x;
+			float nodeToPlayDirX = (nodeToPlayer / nodeToPlayer.magnitude).x;
+			float nodeToNeighDirY = (nodeToNeighbour / nodeToPlayer.magnitude).y;
+			float nodeToPlayDirY = (nodeToPlayer / nodeToPlayer.magnitude).y;
+
+			if(dist < distToTarget ) {
+				distToTarget = dist;
+				closestNode = path [path.Count - 1].neighbour [i];
+			}
+		}
+
+		if (closestNode != null && !closestNode.getInPath ()) {
+			closestNode.setInPath (true);
+			path [path.Count - 1].setColour (Color.yellow);
+			path.Add (closestNode);
+			path [path.Count - 1].setColour (Color.red);
+		} else if (closestNode != null && closestNode.getInPath ()) {
+			closestNode.setColour (Color.magenta);
+			closestNode.setInPath (false);
+			path.RemoveAt (path.Count - 1);
 		}
 	}
 }
