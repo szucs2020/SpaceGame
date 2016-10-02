968df7e0c147a9cf51c65efa5b77b8e7174cbb43
Nigel Martinez <nigel-martinez@hotmail.com>
2016-10-02 17:57:57 -0400

    Temporary menu and "pause menu" implemented.
    Added a menu that is mostly just a scene navigator. The pause menu can
    be accessed in the Main scene by pressing Escape, which puts up an
    overlay and doesn't pause the game (I'll look into pausing local games
    but not network games).
    
    The exit option in the menus only closes the game if you build it, so if
    you try using it in Unity's viewer, it won't work.

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+    CanvasGroup menu;
+    bool active = false;
+
+	// Use this for initialization
+	void Start () {
+        menu = this.gameObject.GetComponent<CanvasGroup>();
+        menu.alpha = 0;
+        menu.interactable = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (!active) {
+                active = true;
+                menu.alpha = 1;
+                menu.interactable = true;
+            } else {
+                active = false;
+                menu.alpha = 0;
+                menu.interactable = false;
+            }
+        }
+	}
+}
