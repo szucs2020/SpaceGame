5707a3e6e0a9012ae2225129cb836ae2da8a71b7
Nigel Martinez <nigel-martinez@hotmail.com>
2016-10-10 23:43:34 -0400

    Pregame Menu Prototype
    Not much going on except for changing a player's team (indicated by
    colour) in the menu so far. Looking to expand this to include changing
    game mode settings, map selection, and even character customization.
    
    Function over form for now, the end product should look a lot nicer.

---
+++ Assets/Scripts/UI/PregameMenu.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PregameMenu : MonoBehaviour {
+    public Sprite blueHead;
+    public Sprite blueTorso;
+    public Sprite blueLegs;
+
+    public Sprite redHead;
+    public Sprite redTorso;
+    public Sprite redLegs;
+
+    public Sprite yellowHead;
+    public Sprite yellowTorso;
+    public Sprite yellowLegs;
+
+    public Sprite greenHead;
+    public Sprite greenTorso;
+    public Sprite greenLegs;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ChangeTeam (string team) {
+        GameObject head = transform.Find("Head").gameObject;
+        GameObject torso = transform.Find("Torso").gameObject;
+        GameObject legs = transform.Find("Legs").gameObject;
+
+        if (team.ToUpper() == "BLUE") {
+            head.GetComponent<SpriteRenderer>().sprite = blueHead;
+            torso.GetComponent<SpriteRenderer>().sprite = blueTorso;
+            legs.GetComponent<SpriteRenderer>().sprite = blueLegs;
+        } else if (team.ToUpper() == "RED") {
+            head.GetComponent<SpriteRenderer>().sprite = redHead;
+            torso.GetComponent<SpriteRenderer>().sprite = redTorso;
+            legs.GetComponent<SpriteRenderer>().sprite = redLegs;
+        } else if (team.ToUpper() == "YELLOW") {
+            head.GetComponent<SpriteRenderer>().sprite = yellowHead;
+            torso.GetComponent<SpriteRenderer>().sprite = yellowTorso;
+            legs.GetComponent<SpriteRenderer>().sprite = yellowLegs;
+        } else if (team.ToUpper() == "GREEN") {
+            head.GetComponent<SpriteRenderer>().sprite = greenHead;
+            torso.GetComponent<SpriteRenderer>().sprite = greenTorso;
+            legs.GetComponent<SpriteRenderer>().sprite = greenLegs;
+        }
+    }
+}
