138f7c4517f58b9880a1d1bac23d801d1c41197c
polyal <lorant.polya@gmail.com>
2016-10-19 01:43:33 -0400

    Starting Portal Work

---
+++ Assets/Scripts/ParticleEmitter/Portal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("Hit");
+        if (target != null)
+        {
+            other.gameObject.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, 0);
+        }
+        
+    }
+}
