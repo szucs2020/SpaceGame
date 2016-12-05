08b8e70dea6dc66885082528039c25771695c145
polyal <lorant.polya@gmail.com>
2016-12-04 19:06:03 -0500

    Chat still doesnt work
    syncing to get latest

--- Assets/Scripts/Network/Chat.cs
+++ Assets/Scripts/Network/Chat.cs
@@ -5,54 +5,43 @@ using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class Chat : NetworkBehaviour {
-    private GameObject chatArea;
     private Text chatText;
     private InputField chatInput;
     private Player player;
-    private GameObject chatPanel;
-    private List<GameObject> convo;
 
 	// Use this for initialization
 	void Start () {
-        chatArea = GameObject.Find("ChatArea");
-        chatPanel = (GameObject)Resources.Load("ChatPanel");
+        chatText = GameObject.Find("ChatText").GetComponent<Text>();
         chatInput = GameObject.Find("ChatInput").GetComponent<InputField>();
-        convo = new List<GameObject>();
         player = transform.GetComponent<Player>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        string message = chatInput.text;
-        if (!string.IsNullOrEmpty(message.Trim()) && Input.GetKeyDown("return")){
-            GameObject chatBubble = Instantiate(chatPanel);
-            chatBubble.transform.SetParent(chatArea.transform, false);
-            chatText = chatBubble.transform.GetChild(0).GetComponent<Text>();
-            chat(message);
+        string tempMessage = chatInput.text;
+        if (!string.IsNullOrEmpty(tempMessage.Trim()) && Input.GetKeyDown("return")){
+            print("update");
+            PrintMessage(player.playerName + ": " + tempMessage + "\n");
+            //message = player.playerName + ": " + tempMessage + "\n";
             chatInput.text = "";
-            convo.Add(chatBubble);
-        }
-
-        int i = 1;
-        foreach (GameObject chatBubble in convo) {
-            chatBubble.transform.position = chatInput.transform.position + new Vector3(0, 27*i, 0);
-            i++;
         }
 	}
 
-    public void chat(string message) {
-        CmdChat(message);
+    void PrintMessage(string message) {
+        print("Printmessage");
+        CmdPrintMessage(message);
     }
 
     [Command]
-    void CmdChat(string message) {
-        RpcChat(message);
+    void CmdPrintMessage(string message) {
+        print("CmdPrintmessage");
+        RpcPrintMessage(message);
     }
 
     [ClientRpc]
-    void RpcChat(string message) {
-        chatText.text += "  " + player.playerName + ": ";
+    void RpcPrintMessage(string message)
+    {
+        print("RPC");
         chatText.text += message;
-        chatText.text += "\n";
     }
 }
